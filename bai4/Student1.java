04cc9abb914d6ddf30636db932008134fa1ea3f2
DienVuong <vuongdien261@gmail.com>
2022-10-12 04:47:25 +0700

    xay dung lop chi ghi trong java

---
+++ bai4/Student1.java
@@ -0,0 +1,15 @@
+package bai4;
+
+public class Student {
+    private String name;
+    private String classes;
+    Student(){
+
+    }
+    public void setName(String name){
+        System.out.println("John");
+    }
+    public void setClasses (String classes){
+        System.out.println("C02");
+    }
+}
